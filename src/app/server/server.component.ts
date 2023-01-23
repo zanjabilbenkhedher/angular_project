ed12675f991f9c8f88d75e330c475d8e3696f9e5
zanjabilbenkhedher <zanjabilbenkhedher@gmail.com>
2023-01-23 21:50:21 +0100

    last commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,24 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'server-root',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+        color:white;
+    }
+    `],
 })
 export class ServerComponent{
+serverId=10;
+serverStatus="offline";
+servers=['test1','test2']
+    constructor(){
+        this.serverStatus=Math.random() >0.5 ? "online" : "offline";
+
+    }
+    getColor(){
+        return this.serverStatus==="offline" ?'red' : 'green';
+    }
+
 
 }
